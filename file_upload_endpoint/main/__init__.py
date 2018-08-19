3bbe896b3609eae3d4546ef28b8eadf7e178862d
Felix Fennell <felix@felixfennell.co.uk>
2018-08-19 11:11:34 +0100

    Tidying up

--- file_upload_endpoint/main/__init__.py
+++ file_upload_endpoint/main/__init__.py
@@ -94,7 +94,7 @@ def upload_single_restricted_size():
     content_length = request.content_length
     upload_limit = 1024 * 1024  # 1MB
     if content_length is not None and content_length > upload_limit:
-        payload = {'errors': [error_too_large(upload_limit)]}
+        payload = {'errors': [error_too_large(upload_limit, content_length)]}
         abort(make_response(jsonify(payload), HTTPStatus.REQUEST_ENTITY_TOO_LARGE))
 
     common_single_file()
