a8594d90c5818c77b0a3692d0cffbfb10fada966
Felix Fennell <felix@felixfennell.co.uk>
2018-08-25 16:44:28 +0100

    Refactoring errors into a separate module

--- file_upload_endpoint/main/__init__.py
+++ file_upload_endpoint/main/__init__.py
@@ -1,61 +1,11 @@
 from http import HTTPStatus
-from uuid import uuid4
 
 from flask import Blueprint, request, make_response, jsonify, abort, current_app as app
 
+from file_upload_endpoint.main.errors import error_no_file, error_no_file_selection, error_too_large, \
+        error_wrong_mime_type
+
 main = Blueprint('main', __name__)
-
-
-def error_no_file(field):
-    app.logger.warning(f"[{ field }] field missing in request")
-
-    return {
-        'id': uuid4(),
-        'status': HTTPStatus.BAD_REQUEST,
-        'title': f"[{ field }] field missing in request",
-        'detail': 'Check the name of the field and that the request uses multipart/form-data encoding'
-    }
-
-
-def error_no_file_selection(field):
-    app.logger.warning(f"[{ field }] field value is an empty selection")
-
-    return {
-        'id': uuid4(),
-        'status': HTTPStatus.BAD_REQUEST,
-        'title': f"[{ field }] field value is an empty selection",
-        'detail': 'Check the file selected is specified correctly and is a valid file'
-    }
-
-
-def error_too_large(maximum_size, request_size):
-    app.logger.warning(f"Request content length, [{ request_size }], is too great")
-
-    return {
-        'id': uuid4(),
-        'status': HTTPStatus.REQUEST_ENTITY_TOO_LARGE,
-        'title': 'Request content_length is too great',
-        'detail': 'Check the content_length of the request is less than the maximum allowed',
-        'meta': {
-            'maximum_content_length_allowed': maximum_size,
-            'request_content_length': request_size
-        }
-    }
-
-
-def error_wrong_mime_type(valid_mime_types, invalid_mime_type):
-    app.logger.warning(f"File type uploaded, [{ invalid_mime_type }], is not allowed")
-
-    return {
-        'id': uuid4(),
-        'status': HTTPStatus.UNSUPPORTED_MEDIA_TYPE,
-        'title': 'File type uploaded is not allowed',
-        'detail': 'Check the file mime_type is in the list of allowed types',
-        'meta': {
-            'allowed_mime_types': valid_mime_types,
-            'instance_mime_type': invalid_mime_type
-        }
-    }
 
 
 def common_single_file():
